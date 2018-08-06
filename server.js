5bf3fe2e356e6bd51a82ebbc9ef706a2fe69ecdc
Glitch (pumped-canid) <none>
2018-08-06 12:28:57 +0000

    🍰🏫 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,11 +13,19 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+   res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  try {
+    res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  } catch (err) {
+      res.send("Something went wrong...");
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
