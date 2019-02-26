c0724370fc6d58b667b98fd25ffc30a035581471
Gerald Souders <gmsoudersjr@gmail.com>
2019-02-27 03:52:44 +0900

    the code I wrote on glitch

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 // require and use "multer"...
 
@@ -13,6 +15,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res, next)=>{
+console.log(req.file)
+  res.json({
+    name:req.file.originalname,
+    filename:req.file.filename,
+    type:req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
