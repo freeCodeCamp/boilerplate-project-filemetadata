75cab38acccb7baffbee84f08a5f39d70e1cfbd1
Glitch (ozzym1096-project-filemetadata) <none>
2019-05-17 01:35:11 +0000

    Completed(?) Projected

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
+const multer = require("multer");
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+// API Endpoint
+app.post('/api/fileanalyze', multer().single('upfile'), function(req, res) {
+  let file = req.file;
+  res.json({
+    name: file.originalname,
+    size: file.size
+  })
 });
 
 app.listen(process.env.PORT || 3000, function () {
