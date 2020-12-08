9160e02f7a8861cf2e53ae3e3ab7bddae6214319
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2020-12-09 00:30:08 +0100

    working

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  var upfile = req.file;
+  if (typeof upfile === "undefined") {
+    res.json({error: 'file not uploaded'})
+  }
+  else {
+    res.json({
+      name: upfile.originalname,
+      type: upfile.mimetype,
+      size: upfile.size
+    })
+  }
+})
 
 
 
