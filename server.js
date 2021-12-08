b92a0bc15a8ac19147a5c92b274b5a2321032c2b
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-12-08 14:35:52 -0500

    added to git

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
+// const bodyParser = require('body-parser')
 
 var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  res.json({
+  name: req.file.originalname,
+  type: req.file.mimetype,
+  size: req.file.size,
+})
+})
 
 
 const port = process.env.PORT || 3000;
