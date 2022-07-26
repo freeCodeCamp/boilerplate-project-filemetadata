842877f11684c46e88ad644f23d33a55a8de29b7
Leo <64216877+wangrunlin@users.noreply.github.com>
2022-07-27 01:33:39 +0800

    chore(index.js): outdent

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
